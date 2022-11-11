72fcb6b469986ea427c3f8830632ec31f73d9135
Daniel Brinkmann <daniel.brinkmann@aeb.com>
2022-11-11 18:26:27 +0100

    Some functionalities lol

--- src/de/example/iata/IataExchangeRateApplication.java
+++ src/de/example/iata/IataExchangeRateApplication.java
@@ -1,8 +1,12 @@
 package de.example.iata;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,6 +15,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class IataExchangeRateApplication {
 	List<List<String>> exchangeRates = new ArrayList<>();
@@ -43,8 +49,10 @@ public class IataExchangeRateApplication {
 				
     		}
 			for(int i=0; i<exchangeRates.size(); i++) {
-				if(!countries.containsKey(exchangeRates.get(i).get(2))) {
-					countries.put(exchangeRates.get(i).get(2), exchangeRates.get(i).get(0));
+				String c_id = exchangeRates.get(i).get(2);
+				String c_name = exchangeRates.get(i).get(0);
+				if(!countries.containsKey(c_name)) {
+					countries.put(c_name, c_id);
 				}
 			}
 			
@@ -110,21 +118,80 @@ public class IataExchangeRateApplication {
 	}
 	
 	private void enterIataExchangeRate() throws Exception {
-		String currencyIsoCode = getUserInputForStringField("Währung");
-		String country = countries.get(currencyIsoCode.toUpperCase());
-		Date from = getUserInputForDateField("Von");
-		Date to = getUserInputForDateField("Bis");
-		Double exchangeRate = getUserInputForDoubleField("Euro-Kurs für 1 " + currencyIsoCode);
-		List<String> rate = new ArrayList<>();
-		rate.add(country);
-		rate.add(exchangeRate.toString());
-		rate.add(currencyIsoCode.toUpperCase());
-		rate.add(this.dateFormat.format(from));
-		rate.add(this.dateFormat.format(to));
-		System.out.println(rate);
-		exchangeRates.add(rate);
+		boolean validInput = false;
+		while(!validInput) {
+			String currencyIsoCode = getUserInputForStringField("Währung");
+			if(!countries.containsValue(currencyIsoCode.toUpperCase())) {
+				System.out.println("Diese Währung ist im System nicht hinterlegt.");
+				continue;
+			}
+			List<String> allCountriesWithCurrency = new ArrayList<>();
+			for(var entry : countries.entrySet()) {
+				if(entry.getValue().equals(currencyIsoCode.toUpperCase())) {
+					allCountriesWithCurrency.add(entry.getKey());
+				}
+			}
+			Date from = getUserInputForDateField("Von");
+			if(from == null) {
+				continue;
+			} else if(from.after(new Date())) {
+				System.out.println("Dieses Datum liegt in der Zukunft. Bitte geben Sie ein valides Datum ein");
+				continue;
+			}
+			Date to = getUserInputForDateField("Bis");
+			if(to == null) {
+				continue;
+			} else if(to.before(from)) {
+				System.out.println("Dieses Datum liegt vor dem Anfangsdatum. Bitte geben Sie ein Datum ein, welches nach dem Anfangsdatum liegt.");
+				continue;
+			}
+			Double exchangeRate = getUserInputForDoubleField("Euro-Kurs für 1 " + currencyIsoCode);
+			for(int i=0;i<allCountriesWithCurrency.size();i++) {
+				List<String> rate = new ArrayList<>();
+				rate.add(allCountriesWithCurrency.get(i));
+				rate.add(exchangeRate.toString().replace('.', ','));
+				rate.add(currencyIsoCode.toUpperCase());
+				rate.add(this.dateFormat.format(from));
+				rate.add(this.dateFormat.format(to));
+				rate.add("");
+				rate.add("");
+
+				exchangeRates.add(rate);
+				writeToCSV(rate);
+
+				System.out.println("Neuen Währungskurs hinzugefügt: "+rate);
+			}
+			validInput = true;
+		}
+		
+		
 		
 		//TODO: Aus den Variablen muss jetzt ein Kurs zusammengesetzt und in die eingelesenen Kurse eingefügt werden. 	--Done
+	}
+
+	private void writeToCSV(List<String> rate) throws IOException {
+		String[] data = rate.toArray(new String[0]);
+		List<String[]> newRate = new ArrayList<>();
+		newRate.add(data);
+		File csvOutputFile = new File("src/de/example/iata/KursExport.csv");
+    	try (PrintWriter pw = new PrintWriter(new FileOutputStream(csvOutputFile, true))) {
+        	newRate.stream()
+          	.map(this::convertToCSV)
+          	.forEach(pw::println);
+    	}
+	}
+
+	private String convertToCSV(String[] rate) {
+		return Stream.of(rate).map(this::escapeSpecialCharacters).collect(Collectors.joining(";"));
+	}
+
+	private String escapeSpecialCharacters(String data) {
+		String escapedData = data.replaceAll("\\R", " ");
+		if (data.contains(";") || data.contains("\"") || data.contains("'")) {
+			data = data.replace("\"", "\"\"");
+			escapedData = "\"" + data + "\"";
+		}
+		return escapedData;
 	}
 	
 	private String getUserInputForStringField(String fieldName) throws Exception {
@@ -135,14 +202,20 @@ public class IataExchangeRateApplication {
 	private Date getUserInputForDateField(String fieldName) throws Exception {
 		System.out.print(fieldName + " (tt.mm.jjjj): ");
 		String dateString = getUserInput();
-		return this.dateFormat.parse(dateString);
+		long count = dateString.chars().filter(ch -> ch == '.').count();
+		if(count != 2) {
+			System.out.println("Bitte geben Sie ein valides Datum ein");
+			return null;
+		}
+		Date res =  this.dateFormat.parse(dateString);
+		return res;
 	}
 	
 	private Double getUserInputForDoubleField(String fieldName) throws Exception {
 		String doubleString = getUserInputForStringField(fieldName);
 		if(doubleString.indexOf(',') != -1) {
 			doubleString = doubleString.replace(',', '.');
-		}
+		 }
 		return Double.valueOf(doubleString);
 	}
 }
